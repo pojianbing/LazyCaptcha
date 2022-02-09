3157b7349110172c31f0bda3345ebb2bbd0b844b
pojianbing <76243339@qq.com>
2022-02-09 22:43:04 +0800

    fix

---
+++ Lazy.Captcha.Core/ColorManager.cs
@@ -0,0 +1,24 @@
+﻿using SixLabors.ImageSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lazy.Captcha.Core
+{
+    public class ColorManager
+    {
+        private static int[,] COLORS = new int[,] { { 0, 135, 255 }, { 51, 153, 51 }, { 255, 102, 102 }, { 255, 153, 0 }, { 153, 102, 0 }, { 153, 102, 153 }, { 51, 153, 153 }, { 102, 102, 255 }, { 0, 102, 204 }, { 204, 51, 51 }, { 0, 153, 204 }, { 0, 51, 102 } };
+
+        public static Color GetRandomColor()
+        {
+            var random = new Random();
+            var colorIndex = random.Next(COLORS.GetUpperBound(0));
+            var r = (byte)COLORS[colorIndex, 0];
+            var g = (byte)COLORS[colorIndex, 1];
+            var b = (byte)COLORS[colorIndex, 2];
+            return new Color(new SixLabors.ImageSharp.PixelFormats.Rgb24(r, g, b));
+        }
+    }
+}
