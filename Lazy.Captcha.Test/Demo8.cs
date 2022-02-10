777eb13b175c2d4cb453b66be7edd75b5c882c2a
pojianbing <houruide@qq.com>
2022-02-10 15:33:23 +0800

    fix

--- Lazy.Captcha.Test/Demo8.cs
+++ Lazy.Captcha.Test/Demo8.cs
@@ -1,4 +1,5 @@
 ﻿using Lazy.Captcha.Core;
+using SixLabors.Fonts;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Drawing;
 using SixLabors.ImageSharp.Drawing.Processing;
@@ -20,33 +21,71 @@ namespace Lazy.Captcha.Test
             int width = 130;
             int height = 48;
 
-            var random = new Random();
+
 
             using Image<Rgba32> img = new(width, height, Color.White);
 
-            for (int i = 0; i < 3; i++)
+            img.Mutate(ctx =>
             {
-                //g.setColor(color == null ? color() : color);
-                //int w = 5 + num(10);
-                //g.drawOval(num(width - 25), num(height - 15), w, w);
-                img.Mutate(ctx =>
+                ctx.SetGraphicsOptions(new GraphicsOptions()
                 {
-                    ctx.SetGraphicsOptions(new GraphicsOptions()
-                    {
-                        Antialias = true
-                    });
-
-                    var color = ColorManager.GetRandomColor();
-                    var w = 5 + random.Next(10);
-                    var point = new PointF(random.Next(width - 25) + w, random.Next(height - 15) + w);
-                    var size = new SizeF(w, w);
-                    var circle = new EllipsePolygon(point, size);
-
-                    ctx.Draw(color, 1, circle.Clip());
+                    Antialias = true
                 });
-            }
 
-            img.Save(DateTime.Now.Ticks + ".png");
+                // 绘制气泡
+                DrawBubble(ctx, width, height, 3);
+                // 绘制干扰线
+                DrawInterferenceLine(ctx, width, height);
+                // 绘制文字
+                DrawChinese(ctx, width, height, "我爱天口");
+            });
+
+            img.Save("a.png");
+        }
+
+        static void DrawBubble(IImageProcessingContext ctx, int width, int height, int count)
+        {
+            for (var i = 0; i < count; i++)
+            {
+                DrawBubble(ctx, width, height);
+            }
+        }
+
+        static void DrawBubble(IImageProcessingContext ctx, int width, int height)
+        {
+            var random = new Random();
+            var color = ColorManager.GetRandomColor();
+            var w = 5 + random.Next(10);
+            var point = new PointF(random.Next(width - 25) + w, random.Next(height - 15) + w);
+            var size = new SizeF(w, w);
+            var circle = new EllipsePolygon(point, size);
+            ctx.Draw(color, 1, circle.Clip());
+        }
+
+        static void DrawInterferenceLine(IImageProcessingContext ctx, int width, int height)
+        {
+            var random = new Random();
+            var color = ColorManager.GetRandomColor();
+            int x1 = 5, y1 = random.Next(5, height / 2);
+            int x2 = width - 5, y2 = random.Next(height / 2, height - 5);
+            int ctrlx1 = random.Next(width / 4, width / 4 * 3), ctrly1 = random.Next(5, height - 5);
+            int ctrlx2 = random.Next(width / 4, width / 4 * 3), ctrly2 = random.Next(5, height - 5);
+            ctx.DrawBeziers(color, 1, new PointF(x1, y1), new PointF(ctrlx1, ctrly1), new PointF(ctrlx2, ctrly2), new PointF(x2, y2));
+        }
+
+        static void DrawChinese(IImageProcessingContext ctx, int width, int height, string text)
+        {
+            Font font = new Font(SystemFonts.Families.Last(), 28);
+
+            int fW = width / text.Count(); // 每一个字符宽度
+            int fSp = (int)(fW - TextMeasurer.Measure("王", new RendererOptions(font)).Width) / 2;
+            for (var i = 0; i < text.Count(); i++)
+            {
+                var fontHeight = (int)TextMeasurer.Measure(text[i].ToString(), new RendererOptions(font)).Height;
+                int fY = (height - fontHeight) / 2;  // 文字的纵坐标
+                var color = ColorManager.GetRandomColor();
+                ctx.DrawText(text[i].ToString(), font, color, new PointF(i * fW + fSp + 3, fY + 3));
+            }
         }
     }
 }
