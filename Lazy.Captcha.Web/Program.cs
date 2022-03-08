e8d7587ed4633d64cbc30b41b0233d4241d33697
pojianbing <76243339@qq.com>
2022-03-08 19:52:47 +0800

    修改演示代码注释

--- Lazy.Captcha.Web/Program.cs
+++ Lazy.Captcha.Web/Program.cs
@@ -4,9 +4,6 @@ using Lazy.Captcha.Core.Generator;
 var builder = WebApplication.CreateBuilder(args);
 
 
-//builder.Services.AddRedisCacheCaptcha(builder.Configuration);
-
-
 // 内存存储， 基于appsettings.json配置
 builder.Services.AddCaptcha(builder.Configuration);
 
@@ -21,7 +18,7 @@ builder.Services.AddCaptcha(builder.Configuration);
 
 // -----------------------------------------------------------------------------
 // 全部配置参数，基于代码配置
-//builder.Services.AddMemoryCacheCaptcha(builder.Configuration, option =>
+//builder.Services.AddCaptcha(builder.Configuration, option =>
 //{
 //    option.CaptchaType = CaptchaType.WORD; // 验证码类型
 //    option.CodeLength = 6; // 验证码长度, 要放在CaptchaType设置后.  当类型为算术表达式时，长度代表操作的个数
@@ -45,7 +42,7 @@ builder.Services.AddCaptcha(builder.Configuration);
 //    option.ImageOption.FontFamily = DefaultFontFamilys.Instance.Actionj; // 字体，中文使用kaiti，其他字符可根据喜好设置（可能部分转字符会出现绘制不出的情况）。
 //});
 
-// 注意： appsettings.json配置和手动代码配置两者选其一, 同时配置时代码配置是无法覆盖appsettings.json配置。另外，appsettings.json配置无法设置所有配置项(例如FontFamily )。
+// 注意： appsettings.json配置和代码配置同时设置时，代码配置会覆盖appsettings.json配置。
 
 builder.Services.AddControllers();
 
