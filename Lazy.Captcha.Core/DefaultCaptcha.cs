c097f260283a649dfadc5dd0ab02caa1016776ff
po <po@DESKTOP-VOAJ5C4>
2022-03-14 14:57:18 +0800

    改进DefaultCaptcha,以便支持自定义Captcha

--- Lazy.Captcha.Core/DefaultCaptcha.cs
+++ Lazy.Captcha.Core/DefaultCaptcha.cs
@@ -12,17 +12,26 @@ namespace Lazy.Captcha.Core
 {
     public class DefaultCaptcha : ICaptcha
     {
-        private readonly IOptionsMonitor<CaptchaOptions> _options;
-        private readonly IStorage _storage;
-        private readonly ICaptchaCodeGenerator _captchaCodeGenerator;
-        private readonly ICaptchaImageGenerator _captchaImageGenerator;
+        protected CaptchaOptions _options;
+        protected IStorage _storage;
+        protected ICaptchaCodeGenerator _captchaCodeGenerator;
+        protected ICaptchaImageGenerator _captchaImageGenerator;
 
-        public DefaultCaptcha(IOptionsMonitor<CaptchaOptions> options, IStorage storage)
+        public DefaultCaptcha(IOptionsSnapshot<CaptchaOptions> options, IStorage storage)
         {
-            _options = options;
+            _options = options.Value;
             _storage = storage;
-            _captchaCodeGenerator = new DefaultCaptchaCodeGenerator(options.CurrentValue.CaptchaType);
+
+            ChangeOptions(_options);
+
+            _captchaCodeGenerator = new DefaultCaptchaCodeGenerator(_options.CaptchaType);
             _captchaImageGenerator = new DefaultCaptchaImageGenerator();
+        }
+
+        // 选项更新
+        protected virtual void ChangeOptions(CaptchaOptions options)
+        { 
+            
         }
 
         /// <summary>
@@ -31,11 +40,11 @@ namespace Lazy.Captcha.Core
         /// <param name="captchaId">验证码id</param>
         /// <param name="expirySeconds">缓存时间，未设定则使用配置时间</param>
         /// <returns></returns>
-        public CaptchaData Generate(string captchaId, int? expirySeconds = null)
+        public virtual CaptchaData Generate(string captchaId, int? expirySeconds = null)
         {
-            var (renderText, code) = _captchaCodeGenerator.Generate(_options.CurrentValue.CodeLength);
-            var image = _captchaImageGenerator.Generate(renderText, _options.CurrentValue.ImageOption);
-            expirySeconds = expirySeconds.HasValue ? expirySeconds.Value : _options.CurrentValue.ExpirySeconds;
+            var (renderText, code) = _captchaCodeGenerator.Generate(_options.CodeLength);
+            var image = _captchaImageGenerator.Generate(renderText, _options.ImageOption);
+            expirySeconds = expirySeconds.HasValue ? expirySeconds.Value : _options.ExpirySeconds;
             _storage.Set(captchaId, code, DateTime.Now.AddSeconds(expirySeconds.Value).ToUniversalTime());
 
             return new CaptchaData(captchaId, code, image);
@@ -48,10 +57,10 @@ namespace Lazy.Captcha.Core
         /// <param name="code">用户输入的验证码</param>
         /// <param name="removeIfSuccess">校验成功时是否移除缓存(用于多次验证)</param>
         /// <returns></returns>
-        public bool Validate(string captchaId, string code, bool removeIfSuccess = true)
+        public virtual bool Validate(string captchaId, string code, bool removeIfSuccess = true)
         {
             var val = _storage.Get(captchaId);
-            var comparisonType = _options.CurrentValue.IgnoreCase ? StringComparison.CurrentCultureIgnoreCase : StringComparison.CurrentCulture;
+            var comparisonType = _options.IgnoreCase ? StringComparison.CurrentCultureIgnoreCase : StringComparison.CurrentCulture;
             var success = string.Equals(val, code, comparisonType);
 
             if (!success || (success && removeIfSuccess))
--- Lazy.Captcha.Web/RandomCaptcha.cs
+++ Lazy.Captcha.Web/RandomCaptcha.cs
@@ -7,25 +7,21 @@ using Microsoft.Extensions.Options;
 
 namespace Lazy.Captcha.Web
 {
-    public class RandomCaptcha : ICaptcha
+    /// <summary>
+    /// 随机验证码
+    /// </summary>
+    public class RandomCaptcha : DefaultCaptcha
     {
-        private readonly CaptchaOptions _options;
-        private readonly IStorage _storage;
-        private readonly ICaptchaCodeGenerator _captchaCodeGenerator;
-        private readonly ICaptchaImageGenerator _captchaImageGenerator;
-
-        public RandomCaptcha(IOptionsMonitor<CaptchaOptions> options, IStorage storage)
+        public RandomCaptcha(IOptionsSnapshot<CaptchaOptions> options, IStorage storage) : base(options, storage)
         {
-            _options = options.CurrentValue;
-            _storage = storage;
 
-            RandomOptions(options.CurrentValue);
-
-            _captchaCodeGenerator = new DefaultCaptchaCodeGenerator(_options.CaptchaType);
-            _captchaImageGenerator = new DefaultCaptchaImageGenerator();
         }
 
-        private void RandomOptions(CaptchaOptions options)
+        /// <summary>
+        /// 更新选项
+        /// </summary>
+        /// <param name="options"></param>
+        protected override void ChangeOptions(CaptchaOptions options)
         {
             var random = new Random();
 
@@ -33,7 +29,7 @@ namespace Lazy.Captcha.Web
             var captchaTypes = new CaptchaType[] { CaptchaType.ARITHMETIC, CaptchaType.NUMBER, CaptchaType.WORD_LOWER, CaptchaType.ARITHMETIC_ZH, };
             options.CaptchaType = captchaTypes[random.Next(0, captchaTypes.Length)];
 
-            // 当是算数表达式时，CodeLength是指运算数个数
+            // 当是算数运算时，CodeLength是指运算数个数
             if (options.CaptchaType.IsArithmetic())
             {
                 options.CodeLength = 2;
@@ -55,43 +51,8 @@ namespace Lazy.Captcha.Web
 
             // 动静随机
             options.ImageOption.Animation = random.Next(2) == 0;
-        }
 
-        /// <summary>
-        /// 生成验证码
-        /// </summary>
-        /// <param name="captchaId">验证码id</param>
-        /// <param name="expirySeconds">缓存时间，未设定则使用配置时间</param>
-        /// <returns></returns>
-        public CaptchaData Generate(string captchaId, int? expirySeconds = null)
-        {
-            var (renderText, code) = _captchaCodeGenerator.Generate(_options.CodeLength);
-            var image = _captchaImageGenerator.Generate(renderText, _options.ImageOption);
-            expirySeconds = expirySeconds.HasValue ? expirySeconds.Value : _options.ExpirySeconds;
-            _storage.Set(captchaId, code, DateTime.Now.AddSeconds(expirySeconds.Value).ToUniversalTime());
-
-            return new CaptchaData(captchaId, code, image);
-        }
-
-        /// <summary>
-        /// 校验
-        /// </summary>
-        /// <param name="captchaId">验证码id</param>
-        /// <param name="code">用户输入的验证码</param>
-        /// <param name="removeIfSuccess">校验成功时是否移除缓存(用于多次验证)</param>
-        /// <returns></returns>
-        public bool Validate(string captchaId, string code, bool removeIfSuccess = true)
-        {
-            var val = _storage.Get(captchaId);
-            var comparisonType = _options.IgnoreCase ? StringComparison.CurrentCultureIgnoreCase : StringComparison.CurrentCulture;
-            var success = string.Equals(val, code, comparisonType);
-
-            if (!success || (success && removeIfSuccess))
-            {
-                _storage.Remove(captchaId);
-            }
-
-            return success;
+            // 其他选项...
         }
     }
 }
